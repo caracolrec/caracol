3ceccf805bf4527c34fd7189c2f7b2aa595a5362
michaelmunson1 <michaelmunson1@gmail.com>
2013-11-26 13:16:03 -0800

    Sets up Knex, JournalEntry schema, and knex query skeleton

--- database/dbschemas.js
+++ database/dbschemas.js
@@ -1,12 +1,14 @@
 var express = require('express');
-var caracolDB = require('./dbsetup.js').caracolDB;
+var caracolPG = require('./dbsetup.js').caracolPG;
+
+// require validator
 
 // see https://github.com/michaelmunson1/Ghost/blob/master/core/server/models/post.js    
 // we begin by writing individual models - if overlap is substantial, refactor out a base.js
 
 // Also - write tests for basic functionality.  <-----
 
-module.exports.User = Bookshelf.PG.Model.extend({
+module.exports.User = caracolPG.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
   
@@ -23,13 +25,17 @@ module.exports.User = Bookshelf.PG.Model.extend({
 
   validate: function(){
      //TO DO
-     //need to escape our text
+     //need to escape our text    <-- npm validator
   },
 
   saving: function(){
     this.attributes = this.pick(this.permittedAttributes); //pick: bookshelf provides?
     //need to do this for various fields? do this in validate:
     //this.set('title', this.sanitize('title').trim());
+  },
+
+  creating: function(){
+    //TO DO
   },
 
   // account: function(){
@@ -46,7 +52,9 @@ module.exports.User = Bookshelf.PG.Model.extend({
 });
 
 
-module.exports.Clipping = Bookshelf.PG.Model.extend({
+
+
+module.exports.Clipping = caracolPG.Model.extend({
   
   tableName: 'clippings',
   hasTimestamps: true,
@@ -58,8 +66,9 @@ module.exports.Clipping = Bookshelf.PG.Model.extend({
   },
 
   permittedAttributes: [
-    'id', 'uuid', 'user', 'title', //'slug', //better understand - see ghost API, post.js, ll67-72
-    'created_at', 'upvoteStatus', 'downvoteStatus', 'bookmarkStatus', 'lastBookmarkTime', 'lastUpvoteTime', 'lastDownvoteTime',  // 'last_update' - could also store history of upvotes and downvotes - not in mvp
+    'id', 'uuid', 'user', 'title', 'content', //'slug', //better understand - see ghost API, post.js, ll67-72
+    'created_at',    // can just use native db tstamp?
+    'upvoteStatus', 'downvoteStatus', 'bookmarkStatus', 'lastBookmarkTime', 'lastUpvoteTime', 'lastDownvoteTime',  // 'last_update' - could also store history of upvotes and downvotes - not in mvp
     //'language' - not in mvp
     'word_count','total_pages', 'date_published', 'dek', 
     'lead_image_url',   // need this?
@@ -71,13 +80,60 @@ module.exports.Clipping = Bookshelf.PG.Model.extend({
      //need to escape our text
   },
 
+  creating: function(){
+    //TO DO
+  },
+
   saving: function(){
     this.attributes = this.pick(this.permittedAttributes); //pick: bookshelf provides?
     //need to do this for various fields? do this in validate:
     //this.set('title', this.sanitize('title').trim());
   },
 
-  users: function(){
-    return this.hasMany(User);
+  clippers: function(){
+    return this.belongsToMany(User);
   }
 });
+
+
+module.exports.JournalEntry = caracolPG.Model.extend({
+  
+  tableName: 'JournalEntries',
+  hasTimestamps: true,
+  
+  initialize: function(){
+    this.on('creating', this.creating, this);
+    this.on('saving', this.saving, this);
+    this.on('saving', this.validate, this);
+  },
+
+  permittedAttributes: [
+    'id', 'uuid', 'user_id', 'title', 'content', //'slug', //better understand - see ghost API, post.js, ll67-72
+    'created_at',    // can just use native db tstamp?
+    // not mvp: 'upvoteStatus', 'downvoteStatus', 'bookmarkStatus', 'lastBookmarkTime', 'lastUpvoteTime', 'lastDownvoteTime',  // 'last_update' - could also store history of upvotes and downvotes - not in mvp
+    // not mvp: 'language'  
+    'word_count','total_pages', 'date_published', 'dek', 
+    //  not mvp: 'lead_image_url',   // need this?
+    //  not mvp: 'next_page_id', 'rendered_pages' //how to use - for i < rendered_pages {go to next page} ?
+  ],
+
+  validate: function(){
+     //TO DO     <------<-------<------
+     //need to escape our text
+  },
+
+  creating: function(){
+    //TO DO
+  },
+
+  saving: function(){
+    this.attributes = this.pick(this.permittedAttributes); //pick: bookshelf provides?
+    //need to do this for various fields? do this in validate:
+    //this.set('title', this.sanitize('title').trim());
+  },
+
+  author: function(){
+    return this.BelongsToOne(User);
+  }
+});
+
--- database/dbsetup.js
+++ database/dbsetup.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var Bookshelf  = require('bookshelf');
+var Knex = require('knex');
 var pg = require('pg');
 
-module.exports.caracolDB = Bookshelf.initialize({
+module.exports.caracolPG = Bookshelf.initialize({
   client: 'pg',
   connection: {
     host     : '127.0.0.1',    //need PORT here?
     //ALSO: note - need diff version for deployment (virtual machine?)
+    user     : 'me',
+    password : 'password',
+    database : 'caracolDB',
+    charset  : 'utf8'
+  }
+});
+
+module.exports.caracolKnex = Knex.initialize({
+  client: 'pg',
+  connection: {
+    host     : '127.0.0.1',
     user     : 'me',
     password : 'password',
     database : 'caracolDB',
