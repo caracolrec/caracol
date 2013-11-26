b23112f06abf40657574588d4a7e3190e58de1a5
Rick Cerf <rickcerf@gmail.com>
2013-11-26 11:38:05 -0800

    post request route

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.get('/app/:u/:t/*', function(req, res){
     //query db to see if favorited
 });
 
+<<<<<<< HEAD
 app.options('/*', function(req, res){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -66,6 +67,10 @@ app.options('/*', function(req, res){
 
 app.post('/uri', function(req, res){
   console.log('post received', req.body);
+=======
+app.post('/uri', function(req, res){
+  console.log("post received",req.body);
+>>>>>>> post request route
   params = {
     url: req.body.uri,
     token: token
