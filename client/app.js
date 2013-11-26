0f3b56c40b98296222a63e193fb4f1bf610075b5
Rick Cerf <rickcerf@gmail.com>
2013-11-26 11:38:05 -0800

    post request route

--- client/app.js
+++ client/app.js
@@ -2,9 +2,22 @@
 ** need a route in the server for the script tag url
 */
 
-
-console.log('party');
-
-setTimeout(function(){
-  console.log("time");
-}, 1500);
+(function() {
+  var d = document,
+      l = d.location,
+      t = window.getSelection,
+      k = d.getSelection,
+      x = d.selection,
+      s = String(t ? t(): (k)? k(): (x ? x.createRange().text : '')),
+      e = encodeURIComponent,
+    req = new XMLHttpRequest(),
+   data = JSON.stringify({
+    uri: l.href,
+    title: d.title,
+    clipping: s
+   });
+  console.log("party");
+  req.open('POST', '//localhost:3000/uri', true);
+  req.setRequestHeader('Content-Type', 'application/JSON');
+  req.send(data);
+})();
