2d4c3d03a9fa9ec12c1692eb8ca1287cd45afcaa
ihinsdale <ian@ianhinsdale.com>
2013-11-27 12:09:05 -0800

    setting up to use zerorpc to control python

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Simple setup.sh for configuring Ubuntu 12.04 LTS Azure instance
+
+sudo apt-get update
+sudo apt-get install build-essential
+sudo apt-get install screen
+
+# Install PostgreSQL
+sudo apt-get install postgresql postgresql-contrib
+
+# Install Python dependencies
+sudo apt-get install python-setuptools
+sudo apt-get install python-pip
+sudo apt-get install python-dev
+
+sudo pip install psycopg
+
+sudo pip install -U numpy
+sudo pip install -U pyyaml nltk
+
+sudo apt-get install libzmq-dev
+sudo pip install pyzmq
+sudo apt-get install libevent-dev
+sudo pip install zerorpc
+
+# Install nvm: node-version manager
+# https://github.com/creationix/nvm
+sudo apt-get install -y git
+sudo apt-get install -y curl
+curl https://raw.github.com/creationix/nvm/master/install.sh | sh
+
+# Load nvm and install latest production node
+source $HOME/.nvm/nvm.sh
+# If the shell script quits with an error after trying to execute line 18,
+# just reenter the line yourself, and then the following line:
+nvm install v0.10.22
+nvm use v0.10.22
