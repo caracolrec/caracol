7a95d0864d3f665952ad62244a9b11a2325f6162
adwitz <adam.witzel@gmail.com>
2013-11-26 16:24:30 -0800

    edit app.js to include localhost

--- client/app.js
+++ client/app.js
@@ -1,8 +1,8 @@
 /* bookmarklet will inject a script that loads this page
 ** need a route in the server for the script tag url
 */
-
 (function() {
+  var route;
   var d = document,
       l = d.location,
       t = window.getSelection,
@@ -14,13 +14,20 @@
    data = JSON.stringify({
     uri: l.href,
     title: d.title,
-    clipping: s
+    clipping: s,
    });
+  //must manually toggle for deployment
+  //TODO there's gotta be a beter way
+  //add a listener
+  route = '//localhost:3000/uri';
+  //route = '//caracol.azurewebsites.net/uri';
   console.log("party");
   setTimeout(function(){
     console.log("time");
   }, 1500);
-  req.open('POST', '//caracol.azurewebsites.net/uri', true);
+  document.body.appendChild('<div>whats up</div>');
+  //do the original db query here;
+  req.open('POST', route, true);
   req.setRequestHeader('Content-Type', 'application/JSON');
   req.send(data);
 })();
