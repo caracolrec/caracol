c54f3c971cea29019142b5e03ccc9504435bf355
michaelmunson1 <michaelmunson1@gmail.com>
2013-11-26 20:50:09 -0800

    Correct export behavior of dbschemas, reference dbconfig file for db credentials

--- database/dbclient.js
+++ database/dbclient.js
@@ -2,11 +2,24 @@
 
 var express = require('express');
 var caracolPG = require('./dbsetup.js').caracolPG;
-var knex = require('./dbsetup.js').knex;
+var knex = caracolPG.knex; // require('./dbsetup.js').knex;
+var tables = require('./dbschemas.js');
 
-module.exports.dbInsert = function(json){
-  var ids = [];  // return all 
-  ids = knex('clippins').insert({json.uri, json.title, json.content});   //where('id', 1).select().then(...
-  return ids;
+
+
+exports.dbInsert = dbInsert = function(json){
+
+  new tables.Clipping({title: 'this is a test', content: 'whammy wowie zowie'})
+  .save()
+  .then(function() {
+    console.log('finished saving the clipping');
+  }, function(){
+    console.log('Error saving the clipping')
+  });
+
 }
+
+dbInsert();
+
+
 
--- database/dbsetup.js
+++ database/dbsetup.js
@@ -2,26 +2,29 @@ var express = require('express');
 var Bookshelf  = require('bookshelf');
 var Knex = require('knex');
 var pg = require('pg');
+var credentials = require('./dbconfig.json');
 
 module.exports.caracolPG = Bookshelf.initialize({
   client: 'pg',
   connection: {
-    host     : '127.0.0.1',    //need PORT here?
+    host     : credentials.host,    // when deploying on Azure, say 'credentials.localhost' here
+
+    //need PORT here?
     //ALSO: note - need diff version for deployment (virtual machine?)
-    user     : 'me',
-    password : 'password',
-    database : 'caracolDB',
+    user     : credentials.user,
+    password : credentials.password,
+    database : credentials.database,
     charset  : 'utf8'
   }
 });
 
-module.exports.knex = Knex.initialize({
-  client: 'pg',
-  connection: {
-    host     : '127.0.0.1',
-    user     : 'me',
-    password : 'password',
-    database : 'caracolDB',
-    charset  : 'utf8'
-  }
-});
+// module.exports.knex = Knex.initialize({
+//   client: 'pg',
+//   connection: {
+//     host     : '127.0.0.1',
+//     user     : 'me',
+//     password : 'password',
+//     database : 'caracolDB',
+//     charset  : 'utf8'
+//   }
+// });
