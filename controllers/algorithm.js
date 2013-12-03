f6d6e10f18bf4289a78d25f2a7d6e01fbdcc447b
michaelmunson1 <michaelmunson1@gmail.com>
2013-12-03 12:32:10 -0800

    fetchTokenizedClippings - in Python

--- controllers/algorithm.js
+++ controllers/algorithm.js
@@ -3,8 +3,17 @@ var zerorpc = require("zerorpc");
 var client = new zerorpc.Client();
 client.connect("tcp://127.0.0.1:4242");
 
-exports.removeHTMLAndTokenize = function(clippingId) {
+exports.removeHTMLAndTokenize = removeHTMLAndTokenize = function(clippingId) {
   client.invoke("remove_html_and_tokenize_clipping_content", clippingId, function(error, res, more) {
       console.log(res);
   });  
 };
+
+// tokenizedClippings = [];
+// first_tokenized = 107 ;
+// last_tokenized =  165 ;
+// for(clippingId = first_tokenized; clippingId <= last_tokenized; clippingId++){
+//   tokenizedClippings.push(removeHTMLAndTokenize(clippingId));
+// }; 
+
+// console.log(tokenizedClippings);
--- python/server.py
+++ python/server.py
@@ -11,6 +11,8 @@ logging.basicConfig()
 credentials = json.load(open(os.path.abspath(os.path.join(os.path.dirname(__file__),"../database/dbconfig.json"))))
 
 class RPC(object):
+
+
     def remove_html_and_tokenize_clipping_content(self, clipping_id):
         # Open connection to the database
         conn = psycopg2.connect(host=credentials["host"],database=credentials["database"],user=credentials["user"],password=credentials["password"])
@@ -21,14 +23,34 @@ class RPC(object):
         content_sans_html = BeautifulSoup(content, "lxml").get_text()
         cur.execute("UPDATE clippings SET content_sans_html = (%s) WHERE id = (%s)", (content_sans_html, clipping_id))
         cur.execute("UPDATE clippings SET content_sans_html_tokenized = (%s) WHERE id = (%s)", (nltk.word_tokenize(content_sans_html), clipping_id))
+        cur.execute("SELECT content_sans_html_tokenized FROM clippings")
+        tokenized = cur.fetchone()[3]
         # Save the changes to the database
         conn.commit()
         # Close database connection
         cur.close()
         conn.close()
+
         return "Cleaned the content of HTML and tokenized"
+
+def fetchTokenizedClippings(first_tokenized, last_tokenized):
+    conn = psycopg2.connect(host=credentials["host"],database=credentials["database"],user=credentials["user"],password=credentials["password"])
+    cur = conn.cursor()
+    # Remove html from the content and tokenize
+    ctr = 0;
+
+    for cl_id in range(first_tokenized, last_tokenized):
+      cur.execute("SELECT content_sans_html_tokenized FROM clippings WHERE id=(%s)", ([cl_id]) )
+      tokenizedContent = cur.fetchone()[0]
+      print "\n\n\n\n\n\n" + str(cl_id) + ":\n\n"
+      print tokenizedContent
+      ctr = ctr + 1
+    
+    cur.close()
+    conn.close()
+
+fetchTokenizedClippings(1, 2)
 
 s = zerorpc.Server(RPC())
 s.bind("tcp://0.0.0.0:4242")
 s.run()
-
