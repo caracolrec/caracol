9ef930feb823952474a336e8200a028bedff0048
adwitz <adam.witzel@gmail.com>
2013-12-04 11:41:17 -0800

    add semicolon

--- client/controllers.js
+++ client/controllers.js
@@ -8,7 +8,7 @@ controllers.controller('VoteCtrl', function($scope, VoteService, storage){
 
   $scope.vote = function(vote){
     //grabs uri and vote status
-    var clipping_id
+    var clipping_id;
     var url = (window.location !== window.parent.location) ? document.referrer: document.location;
     var user_id = storage.get('caracolID');
     var clippings = storage.get('clippings' + user_id);
