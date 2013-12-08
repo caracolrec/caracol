933bed0121118439781fc1f02cf7735545d19948
adwitz <adam.witzel@gmail.com>
2013-12-07 17:23:27 -0800

    back to cloud app

--- dist/bookmarklet/script.js
+++ dist/bookmarklet/script.js
@@ -5,7 +5,7 @@
     $('body').prepend('<div class="caracolBookmarklet"' +
     'style="position:fixed;height:0px;width: 100%;' +
     'z-index:99999;margin:0;padding:0"><iframe class="frame" ' +
-    'src="//localhost:3000/script" ' +
+    'src="//caracol.localhost:3000/script" ' +
     'style="background: transparent;position:absolute;' +
     'z-index:999999;width:100%;height:8em;' +
     'border:2px #ffeed8;border-radius:5px;margin:0;padding:0">' +
@@ -14,7 +14,7 @@
     $('body').prepend('<div class="caracolBookmarklet"' +
     'style="position:fixed;height:0px;width: 100%;' +
     'z-index:99999;margin:0;padding:0"><iframe class="frame" ' +
-    'src="//localhost:3000/script" ' +
+    'src="//caracol.localhost:3000/script" ' +
     'style="background: transparent;position:absolute;' +
     'z-index:999999;width:100%;height:8em;' +
     'border:2px #ffeed8;border-radius:5px;margin:0;padding:0">' +
