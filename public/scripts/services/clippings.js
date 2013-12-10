d3bf08996e2487cdbe7bfae4751d1083abca5944
adwitz <adam.witzel@gmail.com>
2013-12-09 17:12:01 -0800

    Add dummy fix for presentation

--- public/scripts/services/clippings.js
+++ public/scripts/services/clippings.js
@@ -9,7 +9,8 @@ angular.module('caracolApp.services')
     batchSize: 10,
     getClippings: function(currentPage) {
       var requestSize;
-      if (service.timeOfLastFetch) {
+      if (false) {
+      // if (service.timeOfLastFetch) {
         if (currentPage <= service.maxPageVisited) {
           console.log('using already loaded clippings');
           var d = $q.defer();
@@ -23,17 +24,17 @@ angular.module('caracolApp.services')
         console.log('need to fetch clippings from db for the first time');
         requestSize = service.batchSize + 1;
       }
-      return FetchService.fetch('clippings', service.lastClippingId, requestSize)
+      return FetchService.fetch('clippings', 0, 11)
+      // return FetchService.fetch('clippings', service.lastClippingId, requestSize)
         .then(function(data) {
           service.updateState(data);
         });
     },
     updateState: function(clippings) {
       service.timeOfLastFetch = new Date().getTime();
-      service.currentClippings = service.currentClippings.concat(clippings);
-      if (service.currentClippings.length) {
-        service.lastClippingId = service.currentClippings[service.currentClippings.length - 1].id;
-      }
+      service.currentClippings = clippings;
+      // service.currentClippings = service.currentClippings.concat(clippings);
+      service.lastClippingId = service.currentClippings[service.currentClippings.length - 1].id;
       console.log('lastId after getting latest batch of clippings:', service.lastClippingId);
       service.maxPageVisited += 1;
     }
