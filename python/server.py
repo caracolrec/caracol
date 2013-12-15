0d46b00f5878200978be042094ce51fd20b6fe02
michaelmunson1 <michaelmunson1@gmail.com>
2013-12-14 22:09:40 -0800

    Successfully returning ranked similarity scores (recommendations) by db_id

--- python/server.py
+++ python/server.py
@@ -97,22 +97,6 @@ pyserver = json.load(open(os.path.abspath(os.path.join(os.path.dirname(__file__)
 #print pyserver
 
 
-MmCorpus_File = './tmp/initCorp.mm'
-DictFile      = './tmp/init.dict'
-SimsFile      = './tmp/similarities.index'
-#LsiFile       = 
-#Tokens_once   = 
-
-# in v2 - explore smart ways to put these in db
-UserDictionaries = './tmp/UserDicts/'   #for now, place individual user dictionary files in this directory
-UserDictDir      = './tmp/UserDicts/'
-UserLSImodels    = 
-UserSimIndices   = 
-
-
-tokens_once_file = {}
-
-
 def filter(clipping):
     stopwords = "a,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
     stopwords = stopwords.split(",")
@@ -209,7 +193,7 @@ class RPC(object):
 
         filtered = filter(tokenized)
 
-        # IN FILE, DEFINE MAPPING BETWEEN CLIPPING_ID_IN_CORPUS and CLIPPING_ID_IN_DB
+       # IN FILE, DEFINE MAPPING BETWEEN CLIPPING_ID_IN_CORPUS and CLIPPING_ID_IN_DB
         # remember to 0-index CL_ID_IN_CORPUS
 
         # THEN DO TESTS
@@ -275,18 +259,6 @@ def read_and_split_summary_line_of_user_corpus(f):
     summary_split = f.readline().split()
     return summary_split
 
-
-#WRITE:
-# FLOW FOR RECEIVING NEW BMARK, PARSING & FILTERING w.r.t. TO ENTIRE CORPUS 
-
-# LOAD USER CORPUS
-# FETCH RECOMMENDATION FOR THIS ARTICLE                  <---  DEFERRED TO LATER VERSION
-# FETCH RECOMMENDATION FOR ALL ARTICLES
-# (COMPUTE & ) STORE RECOMMENDATION FOR THIS ARTICLE     <---  DEFERRED TO LATER VERSION
-# (RECOMPUTE & ) STORE RECOMMENDATIONS FOR ALL ARTICLES  <---  NECESSARY IN THIS VERSION?
-
-
-
 def get_number_of_files_in_user_corpus(f):
     summary_split = read_and_split_summary_line_of_user_corpus(f)
     return int(summary_split[0])
@@ -334,6 +306,7 @@ def format_clipping(tokenized_ids):
     # Remove html from the content and tokenize
 
     counter = 1    # note - documents in corpus are indexed 1-up
+
     for cl_id in tokenized_ids:
       print "\ncl_id:\n"
       print cl_id
@@ -514,7 +487,6 @@ def findSimilaritiesToDocument(corpus, document_bowvector, user_id):      #, cli
 #
 # For now the two types of corpora won't mix - later, personal corpora will be 
 # At that time, we'll have to exclude User A's articles from being recommended to her
-
 
 
 clipping_ids = [429]   #[2, 3, 290])
